c70de5f583958d4c14d1f2e78b2a6b02f99746a3
Toshit Chawda <r58playz@gmail.com>
2026-02-11 16:31:10 -0800

    implement copyFile

--- src/fs.ts
+++ src/fs.ts
@@ -7,40 +7,87 @@ type NodeFs = typeof import("node:fs");
 
 type NodeFsPromises = NodeFs["promises"];
 
+let fsConstants: NodeFs["constants"] = {
+	O_RDONLY: 0,
+	O_WRONLY: 1,
+	O_RDWR: 2,
+	S_IFMT: 61440,
+	S_IFREG: 32768,
+	S_IFDIR: 16384,
+	S_IFCHR: 8192,
+	S_IFBLK: 24576,
+	S_IFIFO: 4096,
+	S_IFLNK: 40960,
+	S_IFSOCK: 49152,
+	O_CREAT: 64,
+	O_EXCL: 128,
+	UV_FS_O_FILEMAP: 0,
+	O_NOCTTY: 256,
+	O_TRUNC: 512,
+	O_APPEND: 1024,
+	O_DIRECTORY: 65536,
+	O_NOATIME: 262144,
+	O_NOFOLLOW: 131072,
+	O_SYMLINK: 2097152, // macos only
+	O_SYNC: 1052672,
+	O_DSYNC: 4096,
+	O_DIRECT: 16384,
+	O_NONBLOCK: 2048,
+	S_IRWXU: 448,
+	S_IRUSR: 256,
+	S_IWUSR: 128,
+	S_IXUSR: 64,
+	S_IRWXG: 56,
+	S_IRGRP: 32,
+	S_IWGRP: 16,
+	S_IXGRP: 8,
+	S_IRWXO: 7,
+	S_IROTH: 4,
+	S_IWOTH: 2,
+	S_IXOTH: 1,
+	F_OK: 0,
+	R_OK: 4,
+	W_OK: 2,
+	X_OK: 1,
+	COPYFILE_EXCL: 1,
+	COPYFILE_FICLONE: 2,
+	COPYFILE_FICLONE_FORCE: 4
+};
+
 let Dirent: Pick<NodeFs["Dirent"], keyof NodeFs["Dirent"]> & {
 	new(isSymlink: boolean, isDir: boolean, name: string | Buffer, parentPath: string): any;
 } = class Dirent {
-	#isDir: boolean;
-	#isSymlink: boolean;
-	#name: string | Buffer;
-	#parentPath: string;
+		#isDir: boolean;
+		#isSymlink: boolean;
+		#name: string | Buffer;
+		#parentPath: string;
 
-	constructor(isSymlink: boolean, isDir: boolean, name: string | Buffer, parentPath: string) {
-		this.#isSymlink = isSymlink;
-		this.#isDir = isDir;
-		this.#name = name;
-		this.#parentPath = parentPath;
-	}
+		constructor(isSymlink: boolean, isDir: boolean, name: string | Buffer, parentPath: string) {
+			this.#isSymlink = isSymlink;
+			this.#isDir = isDir;
+			this.#name = name;
+			this.#parentPath = parentPath;
+		}
 
-	isFile() {
-		return !this.#isDir;
-	}
-	isDirectory() {
-		return this.#isDir;
-	}
-	isBlockDevice() { return false; }
-	isCharacterDevice() { return false; }
-	isFIFO() { return false; }
-	isSocket() { return false; }
-	isSymbolicLink() { return this.#isSymlink; }
+		isFile() {
+			return !this.#isDir;
+		}
+		isDirectory() {
+			return this.#isDir;
+		}
+		isBlockDevice() { return false; }
+		isCharacterDevice() { return false; }
+		isFIFO() { return false; }
+		isSocket() { return false; }
+		isSymbolicLink() { return this.#isSymlink; }
 
-	get name() {
-		return this.#name;
+		get name() {
+			return this.#name;
+		}
+		get parentPath() {
+			return this.#parentPath;
+		}
 	}
-	get parentPath() {
-		return this.#parentPath;
-	}
-}
 
 let promisesToDepromisify: Omit<NodeFsPromises, "watch" | "glob" | "constants"> = {
 	async readFile(path, options) {
@@ -215,15 +262,37 @@ let promisesToDepromisify: Omit<NodeFsPromises, "watch" | "glob" | "constants"> 
 			descendants_only: false,
 		})
 		if (!options.force && !ok) throw new Error(decode(u8array).message);
+	},
+	async copyFile(src, dest, mode) {
+		if (typeof src !== "string") throw new Error("TODO");
+		if (typeof dest !== "string") throw new Error("TODO");
+
+		mode ??= 0;
+		let overwrite = (mode & fsConstants.COPYFILE_EXCL) === 0;
+
+		if (mode & fsConstants.COPYFILE_FICLONE_FORCE) throw new Error("copy on write not supported");
+
+		let destName = nodePath.basename(dest);
+		let destDir = nodePath.dirname(dest);
+		let [ok, u8array] = await fetchPuter("copy", undefined, {
+			source: src,
+			destination: destDir,
+			new_name: destName,
+			overwrite,
+			dedupe_name: false,
+		});
+
+		if (!ok) throw new Error(decode(u8array).message);
 	}
 };
-let promisesRemaining: Pick<NodeFsPromises, "watch" | "glob" | "constants"> = {};
+let promisesRemaining: Pick<NodeFsPromises, "watch" | "glob" | "constants"> = { constants: { ...fsConstants } };
 let promises: NodeFsPromises = {} as any;
 Object.assign(promises, promisesToDepromisify, promisesRemaining);
 
 export default {
 	Dirent: Dirent as any,
-	promises, 
+	constants: fsConstants,
+	promises,
 	...(depromisify(promisesToDepromisify))
 } satisfies typeof import("node:fs");
 
