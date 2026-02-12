43a29b17154509a989cc5a39cdb25c297374bd71
Toshit Chawda <r58playz@gmail.com>
2026-02-11 16:38:55 -0800

    reorganize fs

--- src/fs.ts
+++ src/fs.ts
@@ -90,62 +90,26 @@ let Dirent: Pick<NodeFs["Dirent"], keyof NodeFs["Dirent"]> & {
 	}
 
 let promisesToDepromisify: Omit<NodeFsPromises, "watch" | "glob" | "constants"> = {
-	async readFile(path, options) {
-		if (typeof path !== "string") throw new Error("TODO");
+	async copyFile(src, dest, mode) {
+		if (typeof src !== "string") throw new Error("TODO");
+		if (typeof dest !== "string") throw new Error("TODO");
 
-		if (typeof options === "string") options = { encoding: options };
-		else if (!options) options = {};
+		mode ??= 0;
+		let overwrite = (mode & fsConstants.COPYFILE_EXCL) === 0;
 
-		// options.flag doesn't do anything?
-		let [ok, u8array] = await fetchPuter(`read?file=${encodeURIComponent(path)}`, options.signal);
+		if (mode & fsConstants.COPYFILE_FICLONE_FORCE) throw new Error("copy on write not supported");
+
+		let destName = nodePath.basename(dest);
+		let destDir = nodePath.dirname(dest);
+		let [ok, u8array] = await fetchPuter("copy", undefined, {
+			source: src,
+			destination: destDir,
+			new_name: destName,
+			overwrite,
+			dedupe_name: false,
+		});
 
 		if (!ok) throw new Error(decode(u8array).message);
-
-		let buf = Buffer.from(u8array);
-		if (options.encoding)
-			// not sure why ts doesn't like this
-			return buf.toString(options.encoding) as any;
-		else
-			return buf;
-	},
-	async writeFile(file, data, options) {
-		if (typeof file !== "string") throw new Error("TODO");
-
-		if (typeof options === "string") options = { encoding: options };
-		else if (!options) options = {};
-
-		// options.flag doesn't do anything?
-
-		let buf;
-		if (typeof data === "string") buf = Buffer.from(data, options.encoding || undefined);
-		else if (data instanceof Buffer) buf = data;
-		else if (data instanceof DataView) buf = Buffer.from(data.buffer);
-		else if (data instanceof streamReadable) buf = await streamToBuffer(data);
-		else if ("buffer" in data) buf = Buffer.from(data.buffer);
-		else throw new Error("TODO");
-
-		let name = nodePath.basename(file);
-		let path = nodePath.dirname(file);
-
-		let [_ok, u8array] = await fetchPuter("batch", options.signal, (form) => {
-			let opId = getRandomId();
-			form.append("operation_id", opId);
-			form.append("fileinfo", JSON.stringify({ name, type: "application/octet-stream", size: buf.byteLength }));
-			form.append("operation", JSON.stringify({
-				op: "write",
-				dedupe_name: false,
-				overwrite: true,
-				operation_id: opId,
-				path,
-				name,
-				item_upload_id: 0,
-			}));
-			form.append("file", new File([buf.buffer], name));
-		});
-		let res = decode(u8array);
-
-		let result = res.results[0];
-		if (result.success === false) throw new Error(result.message);
 	},
 	async mkdir(path, options) {
 		if (typeof path !== "string") throw new Error("TODO");
@@ -222,6 +186,24 @@ let promisesToDepromisify: Omit<NodeFsPromises, "watch" | "glob" | "constants"> 
 			}
 		})
 	},
+	async readFile(path, options) {
+		if (typeof path !== "string") throw new Error("TODO");
+
+		if (typeof options === "string") options = { encoding: options };
+		else if (!options) options = {};
+
+		// options.flag doesn't do anything?
+		let [ok, u8array] = await fetchPuter(`read?file=${encodeURIComponent(path)}`, options.signal);
+
+		if (!ok) throw new Error(decode(u8array).message);
+
+		let buf = Buffer.from(u8array);
+		if (options.encoding)
+			// not sure why ts doesn't like this
+			return buf.toString(options.encoding) as any;
+		else
+			return buf;
+	},
 	async rename(oldPath, newPath) {
 		if (typeof oldPath !== "string") throw new Error("TODO");
 		if (typeof newPath !== "string") throw new Error("TODO");
@@ -235,16 +217,6 @@ let promisesToDepromisify: Omit<NodeFsPromises, "watch" | "glob" | "constants"> 
 			overwrite: false,
 			create_missing_parents: false,
 		});
-		if (!ok) throw new Error(decode(u8array).message);
-	},
-	async unlink(path) {
-		if (typeof path !== "string") throw new Error("TODO");
-
-		let [ok, u8array] = await fetchPuter("delete", undefined, {
-			paths: [path],
-			recursive: false,
-			descendants_only: false,
-		})
 		if (!ok) throw new Error(decode(u8array).message);
 	},
 	async rmdir(path) {
@@ -263,27 +235,55 @@ let promisesToDepromisify: Omit<NodeFsPromises, "watch" | "glob" | "constants"> 
 		})
 		if (!options.force && !ok) throw new Error(decode(u8array).message);
 	},
-	async copyFile(src, dest, mode) {
-		if (typeof src !== "string") throw new Error("TODO");
-		if (typeof dest !== "string") throw new Error("TODO");
+	async writeFile(file, data, options) {
+		if (typeof file !== "string") throw new Error("TODO");
 
-		mode ??= 0;
-		let overwrite = (mode & fsConstants.COPYFILE_EXCL) === 0;
+		if (typeof options === "string") options = { encoding: options };
+		else if (!options) options = {};
 
-		if (mode & fsConstants.COPYFILE_FICLONE_FORCE) throw new Error("copy on write not supported");
+		// options.flag doesn't do anything?
 
-		let destName = nodePath.basename(dest);
-		let destDir = nodePath.dirname(dest);
-		let [ok, u8array] = await fetchPuter("copy", undefined, {
-			source: src,
-			destination: destDir,
-			new_name: destName,
-			overwrite,
-			dedupe_name: false,
+		let buf;
+		if (typeof data === "string") buf = Buffer.from(data, options.encoding || undefined);
+		else if (data instanceof Buffer) buf = data;
+		else if (data instanceof DataView) buf = Buffer.from(data.buffer);
+		else if (data instanceof streamReadable) buf = await streamToBuffer(data);
+		else if ("buffer" in data) buf = Buffer.from(data.buffer);
+		else throw new Error("TODO");
+
+		let name = nodePath.basename(file);
+		let path = nodePath.dirname(file);
+
+		let [_ok, u8array] = await fetchPuter("batch", options.signal, (form) => {
+			let opId = getRandomId();
+			form.append("operation_id", opId);
+			form.append("fileinfo", JSON.stringify({ name, type: "application/octet-stream", size: buf.byteLength }));
+			form.append("operation", JSON.stringify({
+				op: "write",
+				dedupe_name: false,
+				overwrite: true,
+				operation_id: opId,
+				path,
+				name,
+				item_upload_id: 0,
+			}));
+			form.append("file", new File([buf.buffer], name));
 		});
+		let res = decode(u8array);
 
+		let result = res.results[0];
+		if (result.success === false) throw new Error(result.message);
+	},
+	async unlink(path) {
+		if (typeof path !== "string") throw new Error("TODO");
+
+		let [ok, u8array] = await fetchPuter("delete", undefined, {
+			paths: [path],
+			recursive: false,
+			descendants_only: false,
+		})
 		if (!ok) throw new Error(decode(u8array).message);
-	}
+	},
 };
 let promisesRemaining: Pick<NodeFsPromises, "watch" | "glob" | "constants"> = { constants: { ...fsConstants } };
 let promises: NodeFsPromises = {} as any;
@@ -295,134 +295,3 @@ export default {
 	promises,
 	...(depromisify(promisesToDepromisify))
 } satisfies typeof import("node:fs");
-
-/*
-function init_puter_portable(auth) {
-
-	// For form or no body
-	const headers = {
-		"Authorization": auth
-	}
-
-	// For a JSON body
-	const headersAndJSON = {
-		"Authorization": auth,
-		"content-type": "application/json;charset=UTF-8"
-	}
-
-	globalThis.puter_pp = {
-		fs: {
-			read: (path) => {
-				return fetch("https://api.puter.com/read?file=" + encodeURIComponent(path), { headers }).then(res => res.blob())
-			},
-			write: (path, data, options) => {
-				const operation_id = crypto.randomUUID(); // SECURE CONTEXTS ONLY, wont work over http
-
-				// Path parsing
-				let directory = path.split("/");
-				const fileName = directory.pop();
-				directory = directory.join("/");
-
-				if (data instanceof File) {
-					// Passthrough
-				} else if (typeof (data) === "string") {
-					data = new File([data], fileName);
-				} else if ((data instanceof Blob) && !(data instanceof File)) {
-					data = new File([data], fileName);
-				}
-
-				if (!options)
-					options = {};
-
-				const operationInfo = {
-					"op": "write",
-					"dedupe_name": options.dedupeName || options.dedupe_name || false,
-					"overwrite": options.overwrite ? true : false,
-					"operation_id": operation_id,
-					"path": directory,
-					"name": fileName,
-					"item_upload_id": 0
-				};
-
-				// Replicate request exactly as puter.fs.write minus socket_id
-				const writeBatchData = new FormData();
-				writeBatchData.append("operation_id", operation_id);
-				writeBatchData.append("fileinfo", JSON.stringify({ name: fileName, type: "application/octet-stream", size: data.size }));
-				writeBatchData.append("operation", JSON.stringify(operationInfo));
-				writeBatchData.append("file", data);
-				return fetch("https://api.puter.com/batch", { method: "POST", body: writeBatchData, headers })
-			},
-			copy: (source, destination, options) => {
-				if (!options) {
-					options = {};
-				}
-
-				passed_options = {
-					source,
-					destination,
-					overwrite: options.overwrite || undefined,
-					dedupe_name: options.dedupe_name || options.dedupeName || undefined,
-					create_missing_parents: options.create_missing_parents || options.createMissingParents || undefined,
-					new_name: options.new_name || options.new_name || undefined
-				}
-
-				return fetch("https://api.puter.com/copy", { method: "POST", body: JSON.stringify(passed_options), headers: headersAndJSON })
-
-			},
-			readdir: (path) => {
-				return fetch("https://api.puter.com/readdir", { method: "POST", body: JSON.stringify({ path }), headers: headersAndJSON }).then(res => res.json())
-			},
-			stat: (path) => {
-				return fetch("https://api.puter.com/stat", { method: "POST", body: JSON.stringify({ path }), headers: headersAndJSON }).then(res => res.json())
-			},
-			mkdir: (path, options) => {
-				if (!options) {
-					options = {}
-				}
-
-				let parent = path.split("/");
-				const newDir = parent.pop();
-				parent = parent.join("/");
-
-				passed_options = {
-					parent,
-					path: newDir,
-					overwrite: options.overwrite || false,
-					dedupe_name: options.dedupe_name || options.dedupeName || false,
-					create_missing_parents: options.create_missing_parents || options.createMissingParents || false
-				}
-
-				return fetch("https://api.puter.com/mkdir", { method: "POST", body: JSON.stringify(passed_options), headers: headersAndJSON }).then(res => res.json())
-			},
-			rename: (path, new_name) => {
-				return fetch("https://api.puter.com/rename", { method: "POST", body: JSON.stringify({ path, new_name }), headers: headersAndJSON }).then(res => res.json())
-			},
-			move: (source, destinationPath, options) => {
-				if (!options) {
-					options = {}
-				}
-				// Normalize
-				if (destinationPath.endsWith("/")) {
-					destinationPath = destinationPath.slice(0, -1)
-				}
-
-				let destination = destinationPath.split("/");
-				const new_name = destination.pop();
-				destination = destination.join("/");
-
-				passed_options = {
-					destination,
-					source,
-					new_name,
-					overwrite: options.overwrite || undefined,
-					dedupe_name: options.dedupe_name || options.dedupeName || undefined,
-					create_missing_parents: options.create_missing_parents || options.createMissingParents || undefined
-				}
-
-				return fetch("https://api.puter.com/move", { method: "POST", body: JSON.stringify(passed_options), headers: headersAndJSON }).then(res => res.json())
-			},
-
-		}
-	}
-}
-*/
